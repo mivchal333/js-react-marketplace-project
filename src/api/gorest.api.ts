b8844c4d2ff39e0f4eb94d8cf876732dc5ba0bcb
mivchal <mivchal@hotmail.com>
2021-05-29 14:02:01 +0200

    gorest api key

--- src/api/gorest.api.ts
+++ src/api/gorest.api.ts
@@ -40,11 +40,25 @@ interface CategoryApiModel {
 }
 
 let BASIC_URL = 'https://gorest.co.in/public-api';
-const fetchProducts = () => axios.get<ResponsePaginationModel<ProductApiModel>>(BASIC_URL + '/products')
+const API_KEY = process.env.REACT_APP_GOREST_API_KEY;
 
-const fetchProduct = (id: number) => axios.get<ResponseModel<ProductApiModel>>(`${BASIC_URL}/products/${id}`)
+const fetchProducts = () => axios.get<ResponsePaginationModel<ProductApiModel>>(BASIC_URL + '/products', {
+    params: {
+        'access-token': API_KEY
+    }
+})
 
-const fetchCategories = () => axios.get<ResponsePaginationModel<CategoryApiModel>>(BASIC_URL + '/categories')
+const fetchProduct = (id: number) => axios.get<ResponseModel<ProductApiModel>>(`${BASIC_URL} / products /${id}`, {
+    params: {
+        'access-token': API_KEY
+    }
+})
+
+const fetchCategories = () => axios.get<ResponsePaginationModel<CategoryApiModel>>(BASIC_URL + '/categories', {
+    params: {
+        'access-token': API_KEY
+    }
+})
 
 const GorestRepository = {
     fetchProducts,
